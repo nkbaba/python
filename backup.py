f6c74bf8bf805c38e2169d07e776b0553f23672d
Apurva <alonemayank@gmail.com>
2012-12-03 19:19:29 +0530

    Backup Program ver 1

---
+++ backup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Filename: backup.py
+
+import os
+import time
+
+source=['"/home/apurva/Documents"', ' /home/apurva/python']
+
+target_d=' /home/apurva/'
+
+target=target_d+os.sep+time.strftime('%H%M%S')+'.zip'
+
+zip_command="zip -qr {0} {1} ".format(target,''.join(source))
+
+if os.system(zip_command) == 0:
+	print('Success',target)
+else:
+	print('Failed')
